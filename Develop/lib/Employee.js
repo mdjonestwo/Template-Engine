4d6ef75b8dfa319ac26c75329b76f61c43009aea
mdjonestwo <mdjones2@protonmail.com>
2020-11-22 19:12:07 -0500

    employee class created and exported to engineer construct

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -5,5 +5,26 @@ class Employee {
         this.id = id
         this.email = email
     }
+
+    getName() {
+        console.log(`Name: ${this.name}`)
+    }
+
+    getID(){
+        console.log(`ID: ${this.id}`)
+    }
    
+    getEmail(){
+        console.log(`Email: ${this.email}`)
+    }
+
+    getRole(){
+        console.log(this)
+    }
 }
+
+const employee  = new Employee('Jones',' Engineer', "jones@jones.com");
+console.log(employee);
+
+
+module.exports = Employee;
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,14 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(gitHub){
+        super("John", "Engineer", "john@john.com")        
+        this.gitHub = gitHub; 
+    }
+
+}
+
+const john = new Engineer("github.com/john")
+
+console.log(john)
